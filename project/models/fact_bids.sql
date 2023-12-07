90c357a986f7fb08e9e92ac1fc4fd2e173bd0d00
data triunity <datatriunity@gmail.com>
2023-12-07 19:50:37 +0000

    adding bid_ok column

--- project/models/fact_bids.sql
+++ project/models/fact_bids.sql
@@ -3,5 +3,6 @@
     {{dbt_utils.generate_surrogate_key(['VB_BIDS.BID_USER_ID'])}} as USER_KEY,
     replace(to_date(VB_BIDS.BID_DATETIME)::varchar,'-','')::int as BID_DATETIME_KEY,
     VB_BIDS.BID_AMOUNT,
-     VB_BIDS.BID_STATUS
+    VB_BIDS.BID_STATUS,
+    case when  lower(VB_BIDS.BID_STATUS) = 'ok' then 'Y' else 'N' end as OK_BID
     from {{source('VBAY', 'VB_BIDS')}}
