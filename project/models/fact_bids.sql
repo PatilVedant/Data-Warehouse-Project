99af14b7afc8d7ff499f850d5c876a9ba417916d
data triunity <datatriunity@gmail.com>
2023-12-07 21:44:04 +0000

    updated

--- project/models/fact_bids.sql
+++ project/models/fact_bids.sql
@@ -1,7 +1,7 @@
    select BID_ID, 
     {{dbt_utils.generate_surrogate_key(['VB_BIDS.BID_ITEM_ID'])}} as ITEM_KEY,
     {{dbt_utils.generate_surrogate_key(['VB_BIDS.BID_USER_ID'])}} as USER_KEY,
-    replace(to_date(VB_BIDS.BID_DATETIME)::varchar,'-','')::int as BID_DATETIME_KEY,
+    replace(to_date(VB_BIDS.BID_DATETIME)::varchar,'-','')::int as DATETIME_KEY,
     VB_BIDS.BID_AMOUNT,
     VB_BIDS.BID_STATUS,
     case when  lower(VB_BIDS.BID_STATUS) = 'ok' then 'Y' else 'N' end as OK_BID
